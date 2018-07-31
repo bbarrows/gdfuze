1000c81451db6354ac9de23be5dd0792a69770a5
Kratos CICD User <nobody@virtustream.com>
2018-07-31 05:53:00 +0200

    Initial Commit

---
+++ gdfuze/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  gdfuze
+//
+//  Created by bbarrows on 7/31/18.
+//  Copyright © 2018 bbarrows. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
